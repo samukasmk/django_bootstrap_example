f4bcadec0b69bb916fdb9aac1fb1d3eb6071472d
SamukaSMk <samukasmk@gmail.com>
2013-09-28 22:05:52 -0300

    Heisenberg.

--- my_bootstrap_site/urls.py
+++ my_bootstrap_site/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'home', 'my_bootstrap_site.views.Home')
+    url(r'home', 'my_bootstrap_site.views.Home'),
+	url(r'say_my_name', 'my_bootstrap_site.views.SayMyName'),
 )
--- my_bootstrap_site/views.py
+++ my_bootstrap_site/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
 def Home(request):
 	#return HttpResponse('Aloha!')
 
-	return render(request, 'aloha.html', { 'name': 'SamukaSMk', 'gender': 'male'})
+	return render(request, 'aloha.html')
+
+
+def SayMyName(request):
+
+	return render(request, 'content_aloha.html', { 'name': 'Heisenberg', 'gender': 'male'})
